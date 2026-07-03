376bae88730e3f1996f6efb0cc36a38bb3a53358
Gerardo Huaman <u202313990@upc.edu.pe>
2026-07-03 17:05:02 -0500

    Actualizando Dashboard

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from "@angular/common";
-import { Component, OnInit } from "@angular/core";
+import { ChangeDetectorRef, Component, OnInit, ViewChild } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
 import { MatIconModule } from "@angular/material/icon";
-import { MatTableModule } from "@angular/material/table";
+import { MatTableDataSource, MatTableModule } from "@angular/material/table";
 import { ClienteService } from "../../core/services/Cliente-service";
 import { VehiculoService } from "../../core/services/Vehiculo-service";
 import { SimulacionService } from "../../core/services/Simulacion-service";
 import { Router, RouterLink } from "@angular/router";
+import { MatPaginator, MatPaginatorModule } from "@angular/material/paginator";
 
 @Component({
   selector: "app-dashboard",
@@ -18,6 +19,7 @@ import { Router, RouterLink } from "@angular/router";
     MatTableModule,
     MatButtonModule,
     MatIconModule,
+    MatPaginatorModule,
     RouterLink
   ],
   templateUrl: "./dashboard.html",
@@ -28,23 +30,34 @@ export class DashboardComponent implements OnInit {
   totalClientes: number = 0
   totalVehiculos: number = 0
   valorFlotaUSD: number = 0
+
+  dataSource = new MatTableDataSource<any>([])
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator
+
   actividadReciente: any[] = []
 
   constructor(
     private clienteService: ClienteService,
     private vehiculoService: VehiculoService,
     private simulacionService: SimulacionService,
-    private router: Router
+    private router: Router,
+    private cdr: ChangeDetectorRef
   ){}
 
   ngOnInit(): void {
     this.cargarMetricasSistema()
   }
 
+  ngAfterViewInit(){
+    this.dataSource.paginator = this.paginator
+  }
+
   cargarMetricasSistema(): void {
     this.clienteService.list().subscribe({
       next: (clientes: any[]) => {
         this.totalClientes = clientes.length
+        this.cdr.detectChanges()
       },
       error: (err) => console.error('Error al recuperar clientes-vehiculos para el dashboard', err)
     })
@@ -53,15 +66,16 @@ export class DashboardComponent implements OnInit {
       next: (vehiculos) => {
         this.totalVehiculos = vehiculos.length
         this.valorFlotaUSD = vehiculos.reduce((acumulador, v) => acumulador + (v.precio_venta || 0), 0)
+        this.cdr.detectChanges()
       },
       error: (err) => console.error('Error al recuperar vehiculos para el dashboard', err)
     })
 
     this.simulacionService.list().subscribe({
       next: (simulaciones) => {
-        this.actividadReciente = simulaciones
-            .sort((a,b) => b.id - a.id)
-            .slice(0, 5)
+        const ordenadas = simulaciones.sort((a,b) => b.id_simulacion - a.id_simulacion)
+        this.dataSource.data = ordenadas
+        this.cdr.detectChanges()
       },
       error: (err) => console.error('Error al recuperar transacciones recientes', err)
     })
@@ -83,5 +97,18 @@ export class DashboardComponent implements OnInit {
   maskDni(dni: string): string {
     if (!dni || dni.length < 6) return dni || 'No reg.';
     return dni.substring(0, 2) + '***' + dni.substring(dni.length - 3);
+  }
+
+  calcularMontoFinanciado(simulacion: any): number {
+    if(!simulacion || !simulacion.vehiculo) return 0
+
+    const precio = simulacion.vehiculo.precio_venta || 0
+    const porcentajeInicial = simulacion.porcentaje_inicial || 0
+
+    return precio -(precio * (porcentajeInicial / 100))
+  }
+
+  getSimboloMoneda(idMoneda: number): string {
+    return idMoneda === 2 ? '$' : 'S/'
   }
 }
