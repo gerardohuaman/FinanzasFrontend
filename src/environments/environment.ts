ec6e52b298a706e06ab92a03bc7d7f2ab0d19a65
Gerardo Huaman <u202313990@upc.edu.pe>
2026-07-06 13:23:49 -0500

    Otra vez site key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     base: 'https://finanzasbackend-production.up.railway.app',
-    recaptchaSiteKey: '6LcJ90ctAAAAAHCPzASlb9w2Ff6jpJ-eHY8Vo9xn'
+    recaptchaSiteKey: '6Lcm90ctAAAAAJrtP509A0njr99TzduTLIJWsNOx'
 }
